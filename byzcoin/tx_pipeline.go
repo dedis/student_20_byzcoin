6fd7efc745439887243f813ae2bb9d1990faac76
Louis Merlin <louis_merlin1@yahoo.fr>
2020-05-13 11:39:20 +0200

    Modify formating of a comment

--- byzcoin/tx_pipeline.go
+++ byzcoin/tx_pipeline.go
@@ -148,7 +148,6 @@ func (s *defaultTxProcessor) RollupTx() (*rollupTxResult, error) {
 
 	/*
 		//root.Service = s.Service
-	=======
 		// When a block is processed, we prevent conodes to send us back transactions
 		// until the next collection.
 		if !isNotProcessingBlock {
